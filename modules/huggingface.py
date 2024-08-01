7723daf283d6445e82e885d9591d4acda184a8df
wsj-qd-dev <1130287243@qq.com>
2024-08-01 12:38:43 +0000

    feat: 修改模型下载为本地

--- modules/huggingface.py
+++ modules/huggingface.py
@@ -1,4 +1,4 @@
-from transformers import AutoModel
+from transformers import AutoModel,hf_hub_download
 from huggingface_hub import list_repo_files
 import os
 
@@ -11,19 +11,31 @@ hfModelRepoMap = {
     # "/HAT": ""
 }
 def loadHuggingfaceModel(file_path):
-    repoPath = None
+    # repoPath = None
     for pathSuffix in hfModelRepoMap:
         if pathSuffix in file_path:
-            repoPath = f"wsj1995{file_path.split(pathSuffix)[-1]}"
+            # repoPath = f"wsj1995{file_path.split(pathSuffix)[-1]}"
+            filename = file_path.split(pathSuffix)[-1]
+            print({
+                'repo_id':hfModelRepoMap[pathSuffix], 
+                'filename':filename,   
+                'cache_dir':file_path.replace(filename,'')
+            })
+            file_path = hf_hub_download(
+                repo_id=hfModelRepoMap[pathSuffix], 
+                filename=filename,   
+                cache_dir=file_path.replace(filename,'')
+            )
             break
-    if repoPath is None:
-        return repoPath 
-    model = AutoModel.from_pretrained(repoPath,use_auth_token=os.environ.get("HF_TOKEN"))
+    # if repoPath is None:
+    #     return repoPath 
+    # model = AutoModel.from_pretrained(repoPath,use_auth_token=os.environ.get("HF_TOKEN"))
 
-    # 获取模型的状态字典
-    pl_sd = model.state_dict()
+    # # 获取模型的状态字典
+    # pl_sd = model.state_dict()
 
-    return pl_sd
+    # return pl_sd
+    
 
 def huggingfaceModelList(model_path):
     fileList = []
