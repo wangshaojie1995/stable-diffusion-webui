8f2d32fcc7e737c5069de9261fe05e47790f8d94
wsj-qd-dev <1130287243@qq.com>
2024-08-01 12:45:11 +0000

    feat: 修改模型下载

--- modules/huggingface.py
+++ modules/huggingface.py
@@ -1,5 +1,5 @@
-from transformers import AutoModel,hf_hub_download
-from huggingface_hub import list_repo_files
+from transformers import AutoModel
+from huggingface_hub import list_repo_files,hf_hub_download
 import os
 
 hfModelRepoMap = {
@@ -10,21 +10,25 @@ hfModelRepoMap = {
     # "/DAT": "",
     # "/HAT": ""
 }
+# TODO: api运行完成后删除下载的模型
 def loadHuggingfaceModel(file_path):
     # repoPath = None
     for pathSuffix in hfModelRepoMap:
         if pathSuffix in file_path:
             # repoPath = f"wsj1995{file_path.split(pathSuffix)[-1]}"
             filename = file_path.split(pathSuffix)[-1]
+            cacheDir = file_path.replace(filename,'')
             print({
                 'repo_id':hfModelRepoMap[pathSuffix], 
                 'filename':filename,   
-                'cache_dir':file_path.replace(filename,'')
+                'cache_dir':os.path.dirname(file_path),
+                'cacheDir': cacheDir
             })
             file_path = hf_hub_download(
                 repo_id=hfModelRepoMap[pathSuffix], 
                 filename=filename,   
-                cache_dir=file_path.replace(filename,'')
+                cache_dir=os.path.dirname(file_path),
+                token=os.environ.get("HF_TOKEN")
             )
             break
     # if repoPath is None:
