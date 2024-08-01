92af7d58c588c33d2b23b60c8b172b4d85cc6d48
wsj-qd-dev <1130287243@qq.com>
2024-08-01 12:57:06 +0000

    feat: 模型下载

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -298,19 +298,18 @@ def read_metadata_from_safetensors(filename):
 
 def read_state_dict(checkpoint_file, print_global_state=False, map_location=None):
     print(f'-=-=-=-=-=-=- checkpoint_file {checkpoint_file}')
-    pl_sd = loadHuggingfaceModel(checkpoint_file)
-    if pl_sd is None:
-        _, extension = os.path.splitext(checkpoint_file)
-        if extension.lower() == ".safetensors":
-            device = map_location or shared.weight_load_location or devices.get_optimal_device_name()
+    loadHuggingfaceModel(checkpoint_file)
+    _, extension = os.path.splitext(checkpoint_file)
+    if extension.lower() == ".safetensors":
+        device = map_location or shared.weight_load_location or devices.get_optimal_device_name()
 
-            if not shared.opts.disable_mmap_load_safetensors:
-                pl_sd = safetensors.torch.load_file(checkpoint_file, device=device)
-            else:
-                pl_sd = safetensors.torch.load(open(checkpoint_file, 'rb').read())
-                pl_sd = {k: v.to(device) for k, v in pl_sd.items()}
+        if not shared.opts.disable_mmap_load_safetensors:
+            pl_sd = safetensors.torch.load_file(checkpoint_file, device=device)
         else:
-            pl_sd = torch.load(checkpoint_file, map_location=map_location or shared.weight_load_location)
+            pl_sd = safetensors.torch.load(open(checkpoint_file, 'rb').read())
+            pl_sd = {k: v.to(device) for k, v in pl_sd.items()}
+    else:
+        pl_sd = torch.load(checkpoint_file, map_location=map_location or shared.weight_load_location)
 
     if print_global_state and "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
