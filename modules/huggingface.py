5ae1b04251fe14a76404b9bca6e2f43541b61992
wsj-qd-dev <1130287243@qq.com>
2024-08-02 03:07:33 +0000

    fix: 模型下载

--- modules/huggingface.py
+++ modules/huggingface.py
@@ -37,7 +37,7 @@ def loadHuggingfaceModel(file_path):
     for pathSuffix in hfModelRepoMap:
         if pathSuffix in file_path:
             filename = file_path.split(pathSuffix)[-1][1:]
-            pathData = pathlib.Path("/home/user/stable-diffusion-webui/models/Stable-diffusion/100254/111237/hellojplassie_realV10.safetensors")
+            pathData = pathlib.Path(file_path)
             DownLoad(f"https://huggingface.co/{hfModelRepoMap[pathSuffix]}/resolve/main/{filename}", pathData.parent, pathData.name)
             # repoPath = f"wsj1995{file_path.split(pathSuffix)[-1]}"
             # print({
