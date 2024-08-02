bd4a4a02c76770ff2ca25d73272758e84e200384
wsj-qd-dev <1130287243@qq.com>
2024-08-02 02:26:06 +0000

    feat: 修改下载方式

--- modules/huggingface.py
+++ modules/huggingface.py
@@ -2,6 +2,9 @@ from transformers import AutoModel
 from huggingface_hub import list_repo_files,hf_hub_download
 import os
 import shutil
+import subprocess
+import pathlib
+import gc
 
 hfModelRepoMap = {
     "/Stable-diffusion": "wsj1995/Checkpoint",
@@ -11,6 +14,20 @@ hfModelRepoMap = {
     # "/DAT": "",
     # "/HAT": ""
 }
+
+def DownLoad(URI:str,DownloadPath:pathlib.Path,DownLoadFileName:str ) -> int:
+    if (DownloadPath / DownLoadFileName).is_file(): return 0
+    for z in range(10):
+        i=subprocess.run([r"aria2c",r"-c",r"-x" ,r"16", r"-s",r"16", r"-k" ,r"1M" ,r"-m",r"0",r"--enable-mmap=false",r"--console-log-level=error",r"-d",str(DownloadPath),r"-o",DownLoadFileName,URI]);
+        if(i.returncode == 0 ): 
+            del i
+            gc.collect()
+            return 0
+        else :
+            del i
+        raise Exception(str.format("download \'{0}\' failed",URI))
+
+
 # TODO: api运行完成后删除下载的模型
 def loadHuggingfaceModel(file_path):
     if os.path.exists(file_path):
@@ -19,24 +36,26 @@ def loadHuggingfaceModel(file_path):
     # repoPath = None
     for pathSuffix in hfModelRepoMap:
         if pathSuffix in file_path:
-            # repoPath = f"wsj1995{file_path.split(pathSuffix)[-1]}"
+            
             filename = file_path.split(pathSuffix)[-1][1:]
-            print({
-                'repo_id':hfModelRepoMap[pathSuffix], 
-                'filename':filename,   
-                'cache_dir':os.path.dirname(file_path),
-            })
-            downloadedFilePath = hf_hub_download(
-                repo_id=hfModelRepoMap[pathSuffix], 
-                filename=filename,   
-                # cache_dir=os.path.dirname(file_path),
-                token=os.environ.get("HF_TOKEN")
-            )
-            os.makedirs(os.path.dirname(file_path), exist_ok=True) 
-            shutil.move(downloadedFilePath, file_path)
-            print(f'模型文件下载完成 {downloadedFilePath} 并移动到 {file_path}')
+            DownLoad(f"https://huggingface.co/{hfModelRepoMap[pathSuffix]}/{filename}",file_path)
+            # repoPath = f"wsj1995{file_path.split(pathSuffix)[-1]}"
+            # print({
+            #     'repo_id':hfModelRepoMap[pathSuffix], 
+            #     'filename':filename,   
+            #     'cache_dir':os.path.dirname(file_path),
+            # })
+            # downloadedFilePath = hf_hub_download(
+            #     repo_id=hfModelRepoMap[pathSuffix], 
+            #     filename=filename,   
+            #     # cache_dir=os.path.dirname(file_path),
+            #     token=os.environ.get("HF_TOKEN")
+            # )
+            # os.makedirs(os.path.dirname(file_path), exist_ok=True) 
+            # shutil.move(downloadedFilePath, file_path)
+            print(f'模型文件下载完成 {file_path}')
             print(os.path.exists(file_path))
-            print(os.path.exists(downloadedFilePath))
+            # print(os.path.exists(downloadedFilePath))
             break
     # if repoPath is None:
     #     return repoPath 
